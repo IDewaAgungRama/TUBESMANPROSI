b381aa111145a81af3e07324bd61bb55c41024ba
IDewaAgungRama <116362990+IDewaAgungRama@users.noreply.github.com>
2023-11-23 07:54:53 +0700

    Add files via upload

---
+++ TUBESMANPROSI/homepage.php
@@ -0,0 +1,72 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="Homepage" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+
+ 
+<style>
+body {
+    background-color: #CC0303; 
+}
+</style>
+<link rel="stylesheet" href="style.css"> 
+</head>
+
+    <body>
+   
+            <!-- NAVBAR    -->
+                <nav class="Rectangle3 ; stiky-top" style="width: 1396px; height: 87px; left: 60px; top: 50px; position: fixed; opacity: 0.65; background: #F7F6F5; box-shadow: 4px 4px 24px 1px rgba(151, 151, 151, 0.65); border-radius: 10pxz; z-index: 1000;">
+                    <a class="navbar-brand" href="#">
+                    <img src="TEL 1.png" width="150" height="80" class="d-inline-block align-top" alt="">
+                    </a>
+                    <button type="Button" class="btn btn-outline-danger" style="width: 90px; height: 30px; left: 795px; top: 32px; position: absolute; color: #CC0303; font-size: 25.42px; font-family: Inter; font-weight: 500; line-height: 25.42px; word-wrap: break-word"> Home </button>
+                    <a class="About" style="width: 84px; height: 25px; left: 961px; top: 32px; position: absolute; color: black; font-size: 25.42px; font-family: Inter; font-weight: 500; line-height: 25.42px; word-wrap: break-word">About</a>
+                    <a class="Contact" style="width: 106px; height: 25px; left: 1113px; top: 30px; position: absolute; color: black; font-size: 25.42px; font-family: Inter; font-weight: 500; line-height: 25.42px; word-wrap: break-word">Contact</a>
+                    <img src="profil.png" style="width: 60px; height: 50px; left: 1320px; top: 17px; position: absolute">
+                    </div>
+                </nav>
+
+          
+
+               <div class="GreetingsTelkomAdmins" style="width: 423px; left: 78px; top: 374px; position: absolute; color: #F7F6F5; font-size: 48px; font-family: Inter; font-weight: 800; line-height: 52.80px; word-wrap: break-word">Greetings, Telkom Admins! </div>
+                 <div class="YourJourneyAtTelutenStartsHereDiveIntoSeamlessTenantManagementFromContractsToPropertyDetailsWithEaseHappyWorking" style="width: 459px; left: 85px; top: 522px; position: absolute; color: #F7F6F5; font-size: 32px; font-family: Inter; font-weight: 500; line-height: 35.20px; word-wrap: break-word">Your journey at Teluten starts here. Dive into seamless tenant management – from contracts to property details with ease. Happy working!</div>
+                </div>
+    
+                    <img src="orang2.png" style="width: 547px; height: 668px; left: 1000px; top: 232px; position: relative" src="https://via.placeholder.com/687x668" />
+    
+                <div class="WhatSOnYourAgenda" style="left: 545px; top: 1231px; position: absolute; color: #F7F6F5; font-size: 48px; font-family: Inter; font-weight: 800; line-height: 52.80px; word-wrap: break-word">
+                    What's on your agenda?
+                </div>
+                
+
+
+                <div class="kotakfitur" style= "width: 412px; height: 360px; left: 368px; top: 1381px; position: absolute; background: #F7F6F5; box-shadow: 4px 4px 24px 1px rgba(130, 130, 130, 0.60); border-radius: 12px"></div>
+                    <div class="kotakfitur2" style="width: 412px; height: 360px; left: 824px; top: 1793px; position: absolute; background: #F7F6F5; box-shadow: 4px 4px 24px 1px rgba(130, 130, 130, 0.60); border-radius: 12px"></div>
+                        <div class="kotakfitur3" style="width: 412px; height: 360px; left: 368px; top: 1793px; position: absolute; background: #F7F6F5; box-shadow: 4px 4px 24px 1px rgba(130, 130, 130, 0.60); border-radius: 12px"></div>
+                            <div class="kotakfitur4" style="width: 412px; height: 360px; left: 824px; top: 1381px; position: absolute; background: #F7F6F5; box-shadow: 4px 4px 24px 1px rgba(130, 130, 130, 0.60); border-radius: 12px"></div>
+
+                <div class="Location" style="left: 428px; top: 1950px; position: absolute; color: black; font-size: 32px; font-family: Inter; font-weight: 700; line-height: 35.20px; word-wrap: break-word">Location & Facilities</div>
+                        <div class="TenantData" style="left: 478px; top: 1523px; position: absolute; color: black; font-size: 32px; font-family: Inter; font-weight: 700; line-height: 35.20px; word-wrap: break-word">Tenant Data</div>
+                            <div class="Streamline" style="width: 313px; left: 423px; top: 1576px; position: absolute; color: #092C4C; font-size: 20px; font-family: Inter; font-weight: 300; line-height: 22px; word-wrap: break-word">Streamline tenant management – add, monitor, and remove tenants effortlessly.</div>
+                                <div class="EnhanceTenant" style="width: 335px; left: 421px; top: 2005px; position: absolute; color: #092C4C; font-size: 20px; font-family: Inter; font-weight: 300; line-height: 22px; word-wrap: break-word">Enhance tenant satisfaction by offering insights into cafeteria locations and the tenant community size.</div>
+                                        <div class="FinancialDashboard" style="left: 871px; top: 1950px; position: absolute; color: black; font-size: 32px; font-family: Inter; font-weight: 700; line-height: 35.20px; word-wrap: break-word">Financial Dashboard</div>
+                                             <div class="TakeFinancial" style="width: 321px; left: 874px; top: 2005px; position: absolute; color: #092C4C; font-size: 20px; font-family: Inter; font-weight: 300; line-height: 22px; word-wrap: break-word">Take financial control – update payments, analyze targets, and showcase tenant profits all in one dashboard.<br/></div>
+                
+                    <div class="TenantContract" style="left: 913px; top: 1523px; position: absolute; color: black; font-size: 32px; font-family: Inter; font-weight: 700; line-height: 35.20px; word-wrap: break-word">Tenant Contract</div>
+                            <div class="Effortlessly" style="width: 343px; left: 878px; top: 1576px; position: absolute; color: #092C4C; font-size: 20px; font-family: Inter; font-weight: 300; line-height: 22px; word-wrap: break-word">Effortlessly manage tenant contracts – verify extensions, terminations, and initiate new agreements.</div>
+               
+                                <img src="Group 1.png" style="width: 76px; height: 84px; left: 541px; top: 1422px; position: absolute">
+                                <img src="tangan.png" style="width: 88px; height: 96px; left: 982px; top: 1420px; position: absolute">
+                                <img src="Location pin.png" style="width: 80px; height: 80px; left: 533px; top: 1838px; position: absolute">
+                                <img src="Payment.png" style="width: 88px; height: 82px; left: 989px; top: 1843px; position: absolute">
+</div>
+
+</body>
+
+
